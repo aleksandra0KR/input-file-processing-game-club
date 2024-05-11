982efe37b25bd82365f43527ebd9677c92915386
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-05-11 14:08:54 +0300

    feat: added processor for event with ID 11

---
+++ processors/eleventhEvent.go
@@ -0,0 +1,20 @@
+package processors
+
+import (
+	"fmt"
+	"inputfileprocess/model"
+	"os"
+)
+
+func EleventhEvent(event *model.Event, club *model.Club, file *os.File) {
+	line := fmt.Sprintf("%s %d %s\n", event.TimeOfEvent.Format("15:04"), 11, event.ClientID)
+	_, err := file.WriteString(line)
+	if err != nil {
+		fmt.Println("Failed to write to file:", err)
+		os.Exit(1)
+	}
+
+	client, _ := club.Client[event.ClientID]
+
+	delete(club.Client, client.ClientID)
+}
